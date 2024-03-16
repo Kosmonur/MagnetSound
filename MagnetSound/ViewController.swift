d588d05880e3bf9746fca86275b852b5dfa45068
Aleksandr <125129763+Kosmonur@users.noreply.github.com>
2024-03-16 18:09:08 +0300

    Initial Commit

---
+++ MagnetSound/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagnetSound
+//
+//  Created by Александр Пичугин on 16.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
