20b68fad7fd0793cac4db0aa0b74969c257c7d95
Aleksandr <125129763+Kosmonur@users.noreply.github.com>
2024-03-16 22:24:16 +0300

    сделал тестовую версию

--- MagnetSound/ViewController.swift
+++ MagnetSound/ViewController.swift
@@ -1,19 +1,39 @@
-//
-//  ViewController.swift
-//  MagnetSound
-//
-//  Created by Александр Пичугин on 16.03.2024.
-//
-
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    
+    let manager = CMMotionManager()
+    let myUnit = ToneOutputUnit()
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        myUnit.stop()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //        myUnit.setFrequency(freq: 1000)
+        myUnit.setToneVolume(vol: 1)
+        myUnit.enableSpeaker()
+        myUnit.setToneTime(t: 20000)
+        
+        manager.magnetometerUpdateInterval = 0.05
+        manager.startMagnetometerUpdates(to: .main) { [weak self] (data, error) in
+            guard let self else {return}
+            
+            if let x = data?.magneticField.x,
+               let y = data?.magneticField.y,
+               let z = data?.magneticField.z {
+                let freq = 100 + 5*sqrt(x*x+y*y+z*z)
+                myUnit.setFrequency(freq: freq)
+                
+                let red = abs(x.truncatingRemainder(dividingBy: 255)) / 255
+                let green = abs(y.truncatingRemainder(dividingBy: 255)) / 255
+                let blue = abs(z.truncatingRemainder(dividingBy: 255)) / 255
+                
+                view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+            }
+        }
     }
-
-
 }
-
