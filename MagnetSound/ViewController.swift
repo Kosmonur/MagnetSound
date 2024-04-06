e80d55002c45ce18694695a7fe61ae2bf9cef4d4
Aleksandr <125129763+Kosmonur@users.noreply.github.com>
2024-04-06 21:07:20 +0300

    добавил отключение звуаа при удалении магнита

--- MagnetSound/ViewController.swift
+++ MagnetSound/ViewController.swift
@@ -6,6 +6,8 @@ class ViewController: UIViewController {
     let manager = CMMotionManager()
     let myUnit = ToneOutputUnit()
     
+    var baseFreq = 0.0
+    
     override func viewWillDisappear(_ animated: Bool) {
         myUnit.stop()
     }
@@ -13,20 +15,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //        myUnit.setFrequency(freq: 1000)
-        myUnit.setToneVolume(vol: 1)
         myUnit.enableSpeaker()
         myUnit.setToneTime(t: 20000)
         
         manager.magnetometerUpdateInterval = 0.05
+        
         manager.startMagnetometerUpdates(to: .main) { [weak self] (data, error) in
             guard let self else {return}
             
             if let x = data?.magneticField.x,
                let y = data?.magneticField.y,
                let z = data?.magneticField.z {
-                let freq = 100 + 5*sqrt(x*x+y*y+z*z)
-                myUnit.setFrequency(freq: freq)
+                let freq = sqrt(x*x+y*y+z*z)
+                if baseFreq == 0 {
+                    baseFreq = freq + 1
+                }
+                
+                if freq > baseFreq {
+                    myUnit.setFrequency(freq: freq)
+                    myUnit.setToneVolume(vol: freq / 1000)
+                } else {
+                    myUnit.setToneVolume(vol: 0)
+                }
                 
                 let red = abs(x.truncatingRemainder(dividingBy: 255)) / 255
                 let green = abs(y.truncatingRemainder(dividingBy: 255)) / 255
@@ -37,3 +47,4 @@ class ViewController: UIViewController {
         }
     }
 }
+
